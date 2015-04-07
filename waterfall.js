640b9abb5341df5664f4b1a6fc943d8162bf8b6f
Vignesh Shanmugam <vignesh.shanmugam@flipkart.com>
2015-04-07 23:43:30 +0530

    Getting the Max Time

--- waterfall.js
+++ waterfall.js
@@ -34,7 +34,8 @@ import ResTiming from './plugins/restimings';
 
 	var p = w.performance || w.msPerformance || w.webkitPerformance || w.mozPerformance,
 		entries = [],
-		navTiming = NavTiming(d,p);
+		navTiming = NavTiming(d,p),
+		maxTime = 0;
 
 	var table = d.createElement('table');
 
@@ -44,19 +45,26 @@ import ResTiming from './plugins/restimings';
 	}
 	//Resources - Resource Timing
 	entries = ResTiming(p,entries);
-	
+
 	drawWaterfall(entries);
 
 	function drawWaterfall(){
+		getMaxTime();
 		createHeaders();
 		createContainer();
 		// d.body.appendChild(table);
 	}
 
+	function getMaxTime(){
+		entries.map((obj) => {
+			maxTime = Math.max(maxTime, obj.start + obj.duration);
+		});
+	}
+
 	function createHeaders(){
 		var tHead = table.createTHead();
 		var thRow = tHead.insertRow(), thCol;
-		HEADERS.map((v)=>{
+		HEADERS.map((v) => {
 			thCol = thRow.insertCell();
 			thCol.innerHTML = v;
 		});
@@ -64,7 +72,7 @@ import ResTiming from './plugins/restimings';
 
 	function createContainer(){
 		var tBody = d.createElement('tbody');
-		entries.map((o)=>{
+		entries.map((o) => {
 			createResourceGraph(o, tBody);
 		});
 	}
@@ -82,7 +90,26 @@ import ResTiming from './plugins/restimings';
 		var fragment = d.createDocumentFragment();
 		var div = d.createElement('div');
 		var startTime = obj.start || 0;
+		var timeline = getTimelineMetrics(obj);
+		drawTimeLine();
+	}
 
+	//Interval 
+	function getTimelineMetrics(obj){
+		var dnsEle = d.createElement('span');
+		dnsEle.innerHTML = obj.dnsDuration;
+		var tcpEle = d.createElement('span');
+		tcpEle.innerHTML = obj.tcpDuration;
+		var sslEle = d.createElement('span');
+		sslEle.innerHTML = obj.sslDuration;
+		var respEle = d.createElement('span');
+		respEle.innerHTML = obj.responseDuration;
+		var durationEle = d.createElement('span');
+		durationEle.innerHTML = obj.duration;
+	}
+
+	function drawTimeLine(){
+		
 	}
 
 })(window,window.document);
